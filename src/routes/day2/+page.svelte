1f31be2943d4f695b50a98f88b27f19b9b1e9dd9
gursheyss <rkwarya@gmail.com>
2023-12-07 14:50:10 -0800

    day2

---
+++ src/routes/day2/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	let cookieCount = $state(0);
+</script>
+
+<div class="p-4">
+	<Button onclick={() => cookieCount++}>Add</Button>
+	<Button onclick={() => cookieCount--}>Remove</Button>
+	<Button onclick={() => (cookieCount = 0)}>Reset</Button>
+	Cookie Count: {cookieCount}
+</div>
