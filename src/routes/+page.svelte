278416891f851573e261d212f6383dc4f0c78111
gursheyss <rkwarya@gmail.com>
2023-12-07 14:33:47 -0800

    Add dynamic routes to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script lang="ts">
+	const modules = import.meta.glob('./**/+page.svelte');
+	let routes = Object.keys(modules).map((path) => {
+		const day = path.split('/')[1];
+		return {
+			path: `/${day}`,
+			name: day
+		};
+	});
+</script>
+
 <h1>advent of svelte</h1>
-<a href="/day1">day 1</a>
+<div class="flex flex-col">
+	{#each routes as { path, name }}
+		<a class="text-blue-600 underline" href={path}>{name}</a>
+	{/each}
+</div>
