231704d31900679f28e1682d323c74775149a8de
gursheyss <rkwarya@gmail.com>
2023-12-09 22:37:43 -0800

    day6

---
+++ src/routes/day6/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+
+	let beats = $state<number[]>([]);
+
+	let bpm = $derived(
+		beats.length > 1
+			? Math.round(60000 / ((beats[beats.length - 1] - beats[0]) / (beats.length - 1)))
+			: 0
+	);
+</script>
+
+<Button
+	onclick={() => {
+		beats = [...beats, Date.now()];
+	}}>Add Beat</Button
+>
+
+<Button
+	onclick={() => {
+		beats = [];
+	}}>Reset</Button
+>
+
+<p>BPM: {bpm}</p>
